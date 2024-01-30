748fe55cec1e17ce0a377f2a949d4ca0077473d5
Venkitraman <venkitraman@g2techsoft.com>
2024-01-30 17:54:31 +0530

    End of section 5

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,31 +15,33 @@ namespace API.Controllers
 
         [HttpGet("{id}")]  //api/activities/fdefjtrhefhdlk8
 
-        public async Task<ActionResult<Activity>>GetActivity(Guid id)
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             //return await _context.Activities.FindAsync(id);
-            return await Mediator.Send(new Details.Query{Id = id});
+            return await Mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            await Mediator.Send(new Create.Command{Activity = activity});
+            await Mediator.Send(new Create.Command { Activity = activity });
             return Ok();
-        } 
+        }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult>EditActivity(Guid id,Activity activity){
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
             activity.Id = id;
-            await Mediator.Send(new Edit.Command{Activity = activity});
+            await Mediator.Send(new Edit.Command { Activity = activity });
             return Ok();
         }
 
         [HttpDelete("{id}")] //Root parameter
 
-        public async Task<IActionResult>DeleteActivity(Guid id){
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
 
-            await Mediator.Send(new Delete.Command{Id = id});
+            await Mediator.Send(new Delete.Command { Id = id });
             return Ok();
         }
     }
